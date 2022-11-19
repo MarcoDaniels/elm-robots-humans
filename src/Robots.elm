f8adb1e990e32922d24f9b5132ad85718722fa2f
Marco Martins <marcodanielmartins@gmail.com>
2022-11-19 20:26:02 +0100

    Robots builder

--- src/Robots.elm
+++ src/Robots.elm
@@ -1,6 +1,83 @@
 module Robots exposing (..)
 
 
-robots : List String -> String
-robots a =
-    a |> String.concat
+type Path
+    = SinglePath String
+    | MultiPath (List String)
+
+
+type PathAttribute
+    = Sitemap
+    | Allow
+    | Disallow
+
+
+type alias Policy =
+    { userAgent : String
+    , allow : Maybe Path
+    , disallow : Maybe Path
+
+    --    , crawlDelay : Maybe Int -- TODO: make it withCrawlDelay
+    --    , cleanParam : Maybe String -- TODO: make is withCleanParam
+    }
+
+
+type alias Robots =
+    { policies : List Policy
+    , host : String
+    , sitemap : Path
+    }
+
+
+pathAttributeToString : PathAttribute -> String
+pathAttributeToString att =
+    case att of
+        Sitemap ->
+            "Sitemap: "
+
+        Allow ->
+            "Allow: "
+
+        Disallow ->
+            "Disallow: "
+
+
+pathToEntry : Path -> PathAttribute -> String
+pathToEntry pathType attr =
+    case pathType of
+        SinglePath path ->
+            pathAttributeToString attr ++ path
+
+        MultiPath multiple ->
+            multiple
+                |> List.map (\path -> pathAttributeToString attr ++ path)
+                |> String.join ", "
+
+
+robots : Robots -> String
+robots { sitemap, host, policies } =
+    [ policies
+        |> List.map
+            (\policy ->
+                [ "User-agent: " ++ policy.userAgent
+                , case policy.allow of
+                    Just allow ->
+                        pathToEntry allow Allow
+
+                    Nothing ->
+                        ""
+                , case policy.disallow of
+                    Just disallow ->
+                        pathToEntry disallow Disallow
+
+                    Nothing ->
+                        ""
+                ]
+                    |> List.filter (\a -> a /= "")
+                    |> String.join "\n"
+            )
+        |> String.join "\n"
+    , pathToEntry sitemap Sitemap
+    , "Host: " ++ host
+    ]
+        |> String.join "\n"
--- tests/RobotsTest.elm
+++ tests/RobotsTest.elm
@@ -1,15 +1,28 @@
 module RobotsTest exposing (..)
 
 import Expect exposing (equal)
-import Robots exposing (robots)
+import Robots exposing (Path, robots)
 import Test exposing (Test, describe, test)
 
 
 suite : Test
 suite =
     describe "robots"
-        [ test "simple-concat" <|
+        [ test "robots file with one policy & single paths" <|
             \() ->
-                robots [ "a", "b" ]
-                    |> equal "ab"
+                robots
+                    { sitemap = Robots.SinglePath "/sitemap.xml"
+                    , host = "https://marcodaniels.com"
+                    , policies =
+                        [ { userAgent = "*"
+                          , allow = Just (Robots.SinglePath "*")
+                          , disallow = Nothing
+                          }
+                        ]
+                    }
+                    |> equal
+                        """User-agent: *
+Allow: *
+Sitemap: /sitemap.xml
+Host: https://marcodaniels.com"""
         ]
